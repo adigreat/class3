ed6ae56d9f844be06ad08d4f2e41192cd662c0fe
adigreat <53363766+adigreat@users.noreply.github.com>
2019-10-17 18:03:04 +0530

    Add files via upload

--- js/Form.js
+++ js/Form.js
@@ -1,39 +1,35 @@
 class Form {
+
   constructor() {
-  this.input=createInput("Name");
-  this.button=createButton('Play');
-  this.greeting=createElement('h2');  
+    this.input = createInput("Name");
+    this.button = createButton('Play');
+    this.greeting = createElement('h2');
+    this.title = createElement('h2');
   }
   hide(){
     this.greeting.hide();
     this.button.hide();
     this.input.hide();
+    this.title.hide();
   }
 
   display(){
-    var title = createElement('h2')
-    title.html("Car Racing Game");
-    title.position(130, 0);
-    
-   
-    
-    this.input.position(130, 160);
-    this.button.position(250, 200);
+    this.title.html("Car Racing Game");
+    this.title.position(displayWidth/2 - 50, 0);
 
+    this.input.position(displayWidth/2 - 40 , displayHeight/2 - 80);
+    this.button.position(displayWidth/2 + 30, displayHeight/2);
+2
     this.button.mousePressed(()=>{
       this.input.hide();
       this.button.hide();
-
       player.name = this.input.value();
-      
       playerCount+=1;
-      player.index=playerCount;
-
-      player.update()
+      player.index = playerCount;
+      player.update();
       player.updateCount(playerCount);
-      var greeting = createElement('h3');
-      greeting.html("Hello " + player.name )
-      greeting.position(130, 100)
+      this.greeting.html("Hello " + player.name)
+      this.greeting.position(displayWidth/2 - 70, displayHeight/4);
     });
 
   }
--- js/Player.js
+++ js/Player.js
@@ -1,13 +1,13 @@
 class Player {
   constructor(){
-    this.index= null;
-    this.distance= 0;
-    this.name= null
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
   }
 
   getCount(){
     var playerCountRef = database.ref('playerCount');
-    playerCountRef.on("value",function(data){
+    playerCountRef.on("value",(data)=>{
       playerCount = data.val();
     })
   }
@@ -26,10 +26,10 @@ class Player {
     });
   }
 
-static getPlayerInfo(){
-  var playerInfoRef= database.ref('players');
-  playerInfoRef.on("value",(data)=>{
-    allPlayers=data.value();
-  })
-}
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
 }
